643199e987eda1a45a632c012ab897f616bd7eba
chandramouli028 <chandramouli028@gmail.com>
2016-08-22 13:24:12 -0500

    Removed surefire plugin

---
+++ CustomerServiceManager/src/main/java/com/utilityservicemanager/customerservice/registration/webservice/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://webservice.customerservice.utilitywebservice.com/")
+package com.utilityservicemanager.customerservice.registration.webservice;
